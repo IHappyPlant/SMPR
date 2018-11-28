02967c3d0c6a191eb09e8c8907416ecc8fafdbf4
IHappyPlant <43047239+IHappyPlant@users.noreply.github.com>
2018-11-28 14:13:55 +0300

    1NN rework

--- sources/1NN.R
+++ sources/1NN.R
@@ -1,52 +1,28 @@
-dist = function(u, v) { # Метрика
-	sqrt(sum((u - v)^2))
+distance = function(u, v) sqrt(sum((u - v)^2))
+get_distances <- function(xl, z) apply(xl[,1:(ncol(xl)-1)], 1, distance, z)
+sort_objects_by_dist <- function(xl, z) xl[order(get_distances(xl, z)),]
+
+onn <- function(xl, z) sort_objects_by_dist(xl, z)[1, ncol(xl)]
+
+build_class_map <- function(xl) {
+  classifiedObjects <- c()
+  for (i in seq(0, 7, 0.1))
+    for (j in seq(0, 2.5, 0.1))
+      classifiedObjects <- rbind(classifiedObjects, c(i, j, onn(xl, c(i, j))))
+  classifiedObjects
 }
 
-sortObj <- function(xl, z, metricFunction = dist) { 
-# Сортировка объектов выборки по расстоянию до классифицируемого
-	l <- nrow(xl)
-	n <- ncol(xl) - 1
-	distances <- rep(0, l)
-	for (i in 1:l)
-		distances[i] <- c(metricFunction(xl[i, 1:n], z))
-	orderedXL <- xl[order(distances), ]
-	return (orderedXL)
-}
-
-oneNN <- function(xl, z) {
-	orderedXL <- sortObj(xl, z);
-	n <- ncol(orderedXL)
-	class <- orderedXL[1, n]
-	return (class)
-}
-
-buildIrisClassMap <- function(xl) {
-  ox <- seq(0, 7, 0.1)
-  oy <- seq(0, 2.5, 0.1)
-  classifiedObjects <- matrix(NA, length(ox) * length(oy), ncol(xl))
-  cnt <- 1
-  for (i in ox)
-    for (j in oy) {
-      z <- c(i, j)
-      class <- oneNN(xl, z)
-      classifiedObjects[cnt, ] <- c(i, j, class)
-      cnt <- cnt + 1
-    }
-  return (classifiedObjects)
-}
-
-drawPlot <- function(xl, classifiedObjects) {
+draw_plot <- function(xl, classifiedObjects) {
   n <- ncol(classifiedObjects)
-  l <- nrow(classifiedObjects)
   colors = c("setosa" = "red", "versicolor" = "green3", "virginica" = "blue")
-  plot(iris[, 3:4], pch = 21, bg = colors[iris$Species], col = colors[iris$Species], main="Классификация ирисов Фишера методом 1NN", xlab = "Длина лепестка", ylab = "Ширина лепестка", asp = 1)
+  plot(xl[, 1:(n-1)], pch = 21, bg = colors[xl[,n]], col = colors[xl[,n]], xlim = c(0, 7), main="Классификация ирисов Фишера методом 1NN", xlab = "Длина лепестка", ylab = "Ширина лепестка", asp = 1)
   points(classifiedObjects[, 1:(n-1)], pch = 22, col = colors[classifiedObjects[, n]])
 }
 
 main <- function() {
   xl <- iris[, 3:5]
-  classifiedObjects <- buildIrisClassMap(xl)
-  drawPlot(xl, classifiedObjects)
+  classified_objects <- build_class_map(xl)
+  draw_plot(xl, classified_objects)
 }
 
 main()
