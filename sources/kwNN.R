5a940f5356b2d2a3a3584e16f5f7ba048ca8911d
IHappyPlant <43047239+IHappyPlant@users.noreply.github.com>
2018-12-06 13:22:08 +0300

    Small fixes

--- sources/kwNN.R
+++ sources/kwNN.R
@@ -59,7 +59,7 @@ drawPlots <- function(k, q, lOOForK, classifiedObjects) {
   lines(lOOForK, col = "red")  
 }
 
-#main <- function() {
+main <- function() {
   xl <- iris[, 3:5]
   lOOForK <- lOO(xl)
   opt_par <- getOptimalPar(lOOForK)
@@ -68,6 +68,6 @@ drawPlots <- function(k, q, lOOForK, classifiedObjects) {
   q <- opt_par[2] / 10
   classifiedObjects <- getIrisClassMap(xl, k, q)
   drawPlots(k, q, lOOForK, classifiedObjects)
-#}
+}
 
 main()
